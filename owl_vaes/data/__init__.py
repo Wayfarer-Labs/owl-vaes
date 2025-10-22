25409dec3a95dcc95cb14ea26490afa113bd4acd
shahbuland <smatiana1@gmail.com>
2025-10-22 17:42:44 +0000

    add video dir audio loader for future audio prep

--- owl_vaes/data/__init__.py
+++ owl_vaes/data/__init__.py
@@ -43,3 +43,6 @@ def get_loader(data_id: str, batch_size: int, **data_kwargs):
     if data_id == "video_dir_loader":
         from .video_dir_loader import get_loader
         return get_loader(batch_size, **data_kwargs)
+    if data_id == "video_dir_audio_loader":
+        from .video_dir_audio_loader import get_loader
+        return get_loader(batch_size, **data_kwargs)
