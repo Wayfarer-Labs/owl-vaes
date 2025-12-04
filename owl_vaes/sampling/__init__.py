3e0c6d5c7baa0d55bced63a3efecb3112563d213
shahbuland <smatiana1@gmail.com>
2025-12-04 01:28:39 +0000

    x0 sampling

--- owl_vaes/sampling/__init__.py
+++ owl_vaes/sampling/__init__.py
@@ -11,7 +11,7 @@ def get_sample_fn(name="simple"):
 
 @torch.no_grad()
 def flow_sample(model, dummy, z, steps, decoder=None, scaling_factor = 1.0, progress_bar = True, cfg_scale = 1.0):
-    x = torch.randn_like(dummy)
+    x = torch.randn_like(dummy) * model.noise_scale
     ts = torch.ones(len(z), device = z.device, dtype = z.dtype)
 
     # Get null embedding from model if CFG is enabled
@@ -49,4 +49,40 @@ def flow_sample(model, dummy, z, steps, decoder=None, scaling_factor = 1.0, prog
         x = decoder(x)
     x = x.clamp(-1,1)
 
+    return x
+
+@torch.no_grad()
+def x0_sample(model, dummy, z, steps, decoder=None, scaling_factor = 1.0, progress_bar = True, cfg_scale = 1.0):
+    x = torch.randn_like(dummy) * model.noise_scale
+    ts = torch.zeros(len(z), device = z.device, dtype = z.dtype)
+
+    def expand(t):
+        return t.view(-1, 1, 1, 1).expand_as(x)
+
+    # Get null embedding from model if CFG is enabled
+    use_cfg = cfg_scale != 1.0
+    if use_cfg:
+        # Get null embedding - expand to batch size
+        null_emb = model.null_emb.unsqueeze(0).expand(len(z), -1, -1, -1)
+
+
+    dt = 1. / steps
+    for i in tqdm(range(steps), disable = not progress_bar):
+        den = expand(1. - ts).clamp(min=0.05)
+        if use_cfg:
+            pred_x0_cond = model(x, z, ts)
+            pred_x0_uncond = model(x, null_emb, ts)
+
+            pred_x0 = pred_x0_uncond + cfg_scale * (pred_x0_cond - pred_x0_uncond)
+        else:
+            pred_x0 = model(x, z, ts)
+
+        v_pred = (pred_x0 - x) / den
+        x = x + dt * v_pred
+        ts = ts + dt
+    
+    if decoder is not None:
+        x = x.bfloat16() * scaling_factor
+        x = decoder(x)
+    x = x.clamp(-1,1)
     return x
