3ec1dec33d8118cd5247e7d1cfcdb4710dc2d6f2
shahbuland <smatiana1@gmail.com>
2025-12-17 01:41:43 +0000

    add dwt to rec live depth trainer

--- owl_vaes/trainers/rec_live_depth.py
+++ owl_vaes/trainers/rec_live_depth.py
@@ -176,8 +176,7 @@ class RecLiveDepthTrainer(BaseTrainer):
                         metrics.log('l2_loss', l2_loss)
 
                     if dwt_weight > 0.0:
-                        with ctx:
-                            dwt_loss = dwt_loss_fn(batch_rec[:,:3], batch[:,:3]) / accum_steps
+                        dwt_loss = dwt_loss_fn(batch_rec[:,:3], batch[:,:3]) / accum_steps
                         total_loss += dwt_loss * dwt_weight
                         metrics.log('dwt_loss', dwt_loss)
 
