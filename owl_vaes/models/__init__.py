bf9c72cdc2a09b69708bb4ec368c84055f1b7095
shahbuland <smatiana1@gmail.com>
2025-12-26 19:19:43 +0000

    remove old stuff

--- owl_vaes/models/__init__.py
+++ owl_vaes/models/__init__.py
@@ -4,33 +4,12 @@ def get_model_cls(model_id: str) -> Any:
     if model_id == "dcae":
         from .dcae import DCAE
         return DCAE
-    if model_id == "dcae_transformer":
-        from .dcae_transformer import DCAETransformer
-        return DCAETransformer
-    if model_id == "titok":
-        from .titok import TiToKVAE
-        return TiToKVAE
-    if model_id == "titok_vq":
-        from .titok_vq import TiToKVQVAE
-        return TiToKVQVAE
-    if model_id == "dcae_vq":
-        from .dcae_vq import DCVQVAE
-        return DCVQVAE
-    if model_id == "proxy_titok":
-        from .proxy_titok import ProxyTiToKVAE
-        return ProxyTiToKVAE
     if model_id == "diff_dec":
         from .diffdec import DiffusionDecoder
         return DiffusionDecoder
     if model_id == "causal_diffdec":
         from .causal_diffdec import CausalDiffusionDecoder
         return CausalDiffusionDecoder
-    if model_id == "meanflow_dec":
-        from .meanflow import MeanFlowImage
-        return MeanFlowImage
-    if model_id == "audio_ae":
-        from .oobleck import OobleckVAE
-        return OobleckVAE
     if model_id == "tdec":
         from .audio_tdec import AudioTransformerDecoder
         return AudioTransformerDecoder
